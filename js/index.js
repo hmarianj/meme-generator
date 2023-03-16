64f9a409fb5bfc427b22c737007c28facd7f2714
Marian Hernandez <marian.hernandez@aconpy.com>
2023-03-15 22:15:29 -0300

    adding function to edit filters and reset them

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Universal selector
 
 const $ = (selector) => document.querySelector(selector)
+const $$ = (selector) => document.querySelectorAll(selector)
 
 // FUNCTIONS
 //  open image editor
@@ -54,10 +55,50 @@ $("#url").addEventListener("input", () =>{
 
 // Background Color
 
-
 $("#bgColor").addEventListener("input", () => {
     const color = $("#bgColor").value
     $("#memeImage").style.backgroundColor = color
     $("#hexColor").innerText = color
 });
+
+// Background filters 
+
+$("#bgEditColor").addEventListener ('change', () => {
+    $("#memeImage").style.backgroundBlendMode = $("#bgEditColor").value
+})
+
+// Filters range
+
+const filterImg = () => {
+    const brightness = $("#glow").value
+    const opacity = $("#opacity").value
+    const contrast = $("#contrast").value
+    const blur = $("#blur").value
+    const grayscale = $("#greyScale").value
+    const sepia = $("#sepia").value
+    const hueRotate = $("#hue").value
+    const saturate = $("#crowded").value
+    const invert = $("#negativo").value
+    const filterString = `brightness(${brightness}) opacity(${opacity}) contrast(${contrast}%) blur(${blur}px) grayscale(${grayscale}%) sepia(${sepia}%) hue-rotate(${hueRotate}deg) saturate(${saturate}%) invert(${invert})`
+    $("#memeImage").style.filter = filterString
+}
+
+$("#glow").addEventListener("change", filterImg)
+$("#opacity").addEventListener("change", filterImg)
+$("#contrast").addEventListener("change", filterImg)
+$("#blur").addEventListener("change", filterImg)
+$("#greyScale").addEventListener("change", filterImg)
+$("#sepia").addEventListener("change", filterImg)
+$("#hue").addEventListener("change", filterImg)
+$("#crowded").addEventListener("change", filterImg)
+$("#negativo").addEventListener("change", filterImg)
+
+// Reset filter
+
+const resetFilters = () => {
+    $$("section input.inRange").forEach((input) => (input.value = input.getAttribute("value")))
+    filterImg()
+}
+$("section").addEventListener("input", filterImg)
+$("#restFilter").addEventListener("click", resetFilters)
 
